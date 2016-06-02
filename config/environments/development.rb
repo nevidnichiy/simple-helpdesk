5409acefdd62a3d45980c985ea394a00fa34f187
Sergey Nevidnichiy <nevidnichiy@gmail.com>
2016-06-03 00:03:38 +0300

    Changed app layout, added dashboard markup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,4 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -39,8 +34,8 @@ Rails.application.configure do
     domain:               '127.0.0.1:3000',
     user_name:            ENV['SMTP_LOGIN'],
     password:             ENV['SMTP_PASS'],
-    authentication:       :plain}
-
+    authentication:       :plain
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
